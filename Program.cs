7606b06491ea747a6d0eaebac716684e6f8b5b41
David Armstrong <proxie89@gmail.com>
2025-03-27 21:11:25 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿namespace ProjektGenspil
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            
+        {
+           
+
+            Console.WriteLine("|----Projekt Genspil Menu----|");
+            Console.WriteLine("|1.AddBoardGame              |");
+            Console.WriteLine("|2.RemoveBoardGame           |");
+            Console.WriteLine("|3.PrintStockList            |");
+            Console.WriteLine("|4.SearchBoardgameFcritria   |");
+            Console.WriteLine("|5.ViewboardGame             |");
+            Console.WriteLine("|----------------------------|");
+
+            int userChoice = Convert.ToInt32(Console.ReadLine());
+
+            switch (userChoice)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("|----Projekt Genspil Menu----|");
+                        Console.WriteLine("|1.AddBoardGame              |");
+                        Console.WriteLine("|2.RemoveBoardGame           |");
+                        Console.WriteLine("|3.PrintStockList            |");
+                        Console.WriteLine("|4.SearchBoardgameFcritria   |");
+                        Console.WriteLine("|5.ViewboardGame             |");
+                        Console.WriteLine("|----------------------------|");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("|----Projekt Genspil Menu----|");
+                        Console.WriteLine("|1.AddBoardGame              |");
+                        Console.WriteLine("|2.RemoveBoardGame           |");
+                        Console.WriteLine("|3.PrintStockList            |");
+                        Console.WriteLine("|4.SearchBoardgameFcritria   |");
+                        Console.WriteLine("|5.ViewboardGame             |");
+                        Console.WriteLine("|----------------------------|");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("|----Projekt Genspil Menu----|");
+                        Console.WriteLine("|1.AddBoardGame              |");
+                        Console.WriteLine("|2.RemoveBoardGame           |");
+                        Console.WriteLine("|3.PrintStockList            |");
+                        Console.WriteLine("|4.SearchBoardgameFcritria   |");
+                        Console.WriteLine("|5.ViewboardGame             |");
+                        Console.WriteLine("|----------------------------|");
+                        break;
+                    }
+                    case 4:
+                    {
+                        
+                       
+                         Console.WriteLine("|----Projekt Genspil Menu----|");
+                         Console.WriteLine("|1.AddBoardGame              |");
+                         Console.WriteLine("|2.RemoveBoardGame           |");
+                         Console.WriteLine("|3.PrintStockList            |");
+                         Console.WriteLine("|4.SearchBoardgameFcritria   |");
+                         Console.WriteLine("|5.ViewboardGame             |");
+                         Console.WriteLine("|----------------------------|");
+                         break;
+                    }
+                    case 5:
+                    {
+
+
+                        Console.WriteLine("|----Projekt Genspil Menu----|");
+                        Console.WriteLine("|1.AddBoardGame              |");
+                        Console.WriteLine("|2.RemoveBoardGame           |");
+                        Console.WriteLine("|3.PrintStockList            |");
+                        Console.WriteLine("|4.SearchBoardgameFcritria   |");
+                        Console.WriteLine("|5.ViewboardGame             |");
+                        Console.WriteLine("|----------------------------|");
+                        break;
+                    }
+            }
+
+            List<Brætspil> spil = new List<Brætspil>();
+
+        }
+    }
+}
---
+++ brætspil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektGenspil
+{
+    public class brætspil
+    {
+        private string _navn;
+        private char _stand;
+        private int _antalSpillere;
+        private int _antalPåLager;
+        private int _pris;
+
+        public string Navn { get { return _navn; } set { _navn = value; } }
+        public char Stand { get { return _stand; } set { _stand = value; } }
+        public int AntalSpillere { get { return _antalSpillere; } set { _antalSpillere = value; } }
+        public int AntalPåLager { get  { return _antalPåLager;} set {  _antalPåLager = value; } }
+        public int Pris { get { return _pris; } set { _pris = value; } }  
+    }
+
+    public class Variabler (string navn, char stand, int antalSpillere, int antalPåLager, int pris)
+    {
+       /* this._navn = navn;
+        this._stand = stand;
+        this._antalSpillere = antalSpillere;
+        this._antalPåLager = antalPåLager;
+        this._pris = pris;    
+       */
+    }
+}
