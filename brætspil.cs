1761b2b0ca584e4d46a14b84cfdd1ed41a8264d6
David Armstrong <proxie89@gmail.com>
2025-03-28 10:22:47 +0100

    contructor ændring 1.0

--- brætspil.cs
+++ brætspil.cs
@@ -13,21 +13,25 @@ namespace ProjektGenspil
         private int _antalSpillere;
         private int _antalPåLager;
         private int _pris;
+        private string _genre;
 
-        public string Navn { get { return _navn; } set { _navn = value; } }
-        public char Stand { get { return _stand; } set { _stand = value; } }
-        public int AntalSpillere { get { return _antalSpillere; } set { _antalSpillere = value; } }
-        public int AntalPåLager { get  { return _antalPåLager;} set {  _antalPåLager = value; } }
-        public int Pris { get { return _pris; } set { _pris = value; } }  
+        public string navn { get { return _navn; } set { _navn = value; } }
+        public char stand { get { return _stand; } set { _stand = value; } }
+        public int antalSpillere { get { return _antalSpillere; } set { _antalSpillere = value; } }
+        public int antalPåLager { get { return _antalPåLager; } set { _antalPåLager = value; } }
+        public int pris { get { return _pris; } set { _pris = value; } }  
+        public string genre { get { return _genre; } set { _genre = value; } }
+    
+
+    public brætspil(string navn, char stand, int antalSpillere, int antalPåLager, int pris, string genre)
+        {
+            _navn = navn;
+            _stand = stand;
+            _antalSpillere = antalSpillere;
+            _antalPåLager = antalPåLager;
+            _pris = pris;
+            _genre = genre;
+        }
     }
-
-    public class Variabler (string navn, char stand, int antalSpillere, int antalPåLager, int pris)
-    {
-        this._navn = navn;
-        this._stand = stand;
-        this._antalSpillere = antalSpillere;
-        this._antalPåLager = antalPåLager;
-        this._pris = pris;    
-       
 }
 
