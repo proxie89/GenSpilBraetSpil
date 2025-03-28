0e137e944be6c9726e3039feb6402a650d5ac916
Melinda Allen <msal71396@edu.ucl.dk>
2025-03-28 16:23:50 +0100

    Ændring 1

--- Forespørgsel.cs
+++ Forespørgsel.cs
@@ -8,6 +8,21 @@ namespace ProjektGenspil
 {
     internal class Forespørgsel
     {
-        
+        public int Id { get; private set; }
+        public string Kundenavn { get; private set; }
+        public int Tlf { get; private set; }
+        public string Brætspil { get; private set; }
+
+
+        // Opret konstruktør
+        public Forespørgsel(int _id, string _kundenavn, int _tlf, string _brætspil)
+        {
+            Id = _id;
+            Kundenavn = _kundenavn;
+            Tlf = _tlf;
+            Brætspil = _brætspil;
+
+
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace ProjektGenspil
     class Program
     {
         static List<string> names = new List<string>(); // Liste til at gemme navne
+        static List<Forespørgsel> forespørgsel = new List<Forespørgsel>(); //Til forespørgsler
 
         static void Main()
         {
@@ -16,6 +17,7 @@ namespace ProjektGenspil
                 Console.WriteLine("1. Tilføj navn til listen");
                 Console.WriteLine("2. Vis alle navne");
                 Console.WriteLine("3. Slet et navn");
+                Console.WriteLine("4. Til forespørgsel menu");
                 Console.WriteLine("4. Afslut");
                 Console.Write("Vælg en mulighed: ");
 
@@ -33,6 +35,9 @@ namespace ProjektGenspil
                         DeleteBoardGame();
                         break;
                     case "4":
+                        ForespørgselMenu();
+                        break;
+                    case "5":
                         Console.WriteLine("Farvel!");
                         return; // Afslutter programmet
                     default:
@@ -108,6 +113,148 @@ namespace ProjektGenspil
             }
         }
 
+        static void ForespørgselMenu()
+        {
+            while (true)
+            {
+                Console.Clear(); // Rydder skærmen for en ren menuvisning
+                Console.WriteLine("=== Forespørgselmenu ===");
+                Console.WriteLine("1. Opret forespørgsel");
+                Console.WriteLine("2. Se liste med forespørgsler");
+                Console.WriteLine("3. Slet forespørgsel");
+                Console.WriteLine("4. Søg på forespørgsel");
+                Console.WriteLine("5. Afslut");
+                Console.Write("Vælg en mulighed: ");
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        AddForspørgsel();
+                        break;
+                    case "2":
+                        SeListe();
+                        break;
+                    case "3":
+                        SletForespørgsel();
+                        break;
+                    case "4":
+                        SøgForespørgsel();
+                        break;
+                    case "5":
+                        Console.WriteLine("Farvel!");
+                        return; // Afslutter programmet
+                    default:
+                        Console.WriteLine("Ugyldigt valg, prøv igen.");
+                        break;
+                }
+
+                Console.WriteLine("Tryk på en tast for at fortsætte...");
+                Console.ReadKey();
+            }
+        }
+
+        static void AddForspørgsel() //Tilføj en Forespørgsel. Skal tildeles et ID
+        {
+            Console.Clear();
+            Console.WriteLine("Hvor mange forespørgsler vil du indtaste?");
+            int antalFor = int.Parse(Console.ReadLine());
+            for (int i = 0; i < antalFor; i++)
+            {
+                Console.WriteLine("Hvilket spil vil kunden gerne oprette forespørgsel på?");
+                string _brætspil = Console.ReadLine();
+
+                Console.WriteLine("Indtast kundens navn");
+                string _kundenavn = Console.ReadLine();
+
+                Console.WriteLine("Indtast kundens telefonnummer");
+                int _tlf = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Indtast forespørgsels ID" + (i + 1) + ": ");
+                int _id = int.Parse(Console.ReadLine());
+
+                forespørgsel.Add(new Forespørgsel(_id, _kundenavn, _tlf, _brætspil));
+                Console.WriteLine($"Forespørgsel {_id} blev oprettet.\nSpil: {_brætspil}\nNavn: {_kundenavn}\nTlf: {_tlf}\n");
+
+            }
+        }
+
+        /*
+        static void AddKunde()
+        {
+            
+            kunde.Add(new Kunde("Kathrine Jensen", 88776655));
+            kunde.Add(new Kunde("Bo Hansen", 99887766));
+
+            Console.Clear();
+            Console.WriteLine("Indtast kundens fulde navn");
+            string name = Console.ReadLine();
+            Console.WriteLine("Indtast telefonnummer på kunden");
+            int nummer = Convert.ToInt32(Console.ReadLine());
+
+            if ((!string.IsNullOrWhiteSpace(name)))
+            {
+                Kunde.Add(name); //Adder til "Kunde" listen
+                Kunde.Add(nummer);
+                Console.WriteLine($"{name} er tilføjet som kunde");
+            }
+            else
+            {
+                Console.WriteLine("Error. Prøv igen");
+            }
+        }
+        */
+        static void SeListe()
+        {
+            // Udskriv liste med forespørgsler
+            Console.Clear();
+            Console.WriteLine("=== Liste over forspørgsler ===");
+            if (forespørgsel.Count == 0)
+            {
+                Console.WriteLine("Ingen forespørgsler er blevet oprettet endnu.");
+            }
+            else
+            {
+                foreach (var forespørgsels in forespørgsel)
+                {
+                    Console.WriteLine($"ID: {forespørgsels.Id} \nNavn: {forespørgsels.Kundenavn} \nTlf: {forespørgsels.Tlf}" +
+                        $"\nBrætspil: {forespørgsels.Brætspil}");
+
+                }
+            }
+            //static List<Forespørgsel> forespøgsel = new List<Forespørgsel>();
+
+        }
+
+        static void SletForespørgsel()
+        {
+            //Indsæt slet forespørgsel
+        }
+
+        static void SøgForespørgsel()
+        {
+            // tilføj søge funktion
+            //Spørg bruger hvilket ID han leder efter
+            Console.WriteLine("Hvilken forespørgsel leder du efter?");
+            // Indtast brugerens ID
+            int søgtID = int.Parse(Console.ReadLine());
+
+            // Søg på person med det angivne ID
+            Forespørgsel søgtForespørgsel = forespørgsel.Find(f => f.Id == søgtID);
+
+            //Udskriv nu det søgte ID's informationer
+            if (søgtForespørgsel != null)
+            {
+                Console.WriteLine($"Forespørgsel: {søgtForespørgsel.Id} \nKundenavn: {søgtForespørgsel.Kundenavn} \n" +
+                    $"Kundetelefon: {søgtForespørgsel.Tlf} \nSpil: {søgtForespørgsel.Brætspil}");
+            }
+            else
+            {
+                Console.WriteLine("Det indtastede nummer er ikke korrekt");
+            }
+
+        }
     }
 }
 
